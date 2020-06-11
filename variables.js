d3b0dab2754e5373beada957db1f41c187fc060f
Rajat Gupta <rajat295gupta@gmail.com>
2020-06-11 18:17:16 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+const name = "Rajat";
+//name = "Gupta";
+console.log(name);
+
+var city = "Delhi";
+console.log(city);
+
+{
+    let city = "Mumbai";
+    city = "Kolkata";
+    console.log(city);
+}
+console.log(city);
